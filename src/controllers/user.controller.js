6f424fb1517b5146d6b42ff2ddd8768a2beaae1e
irfan baig <ibaig9356@gmail.com>
2026-03-29 05:57:12 +0530

    feat : login / logout controller with route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const userRegister = async (req, res) => {
@@ -43,6 +44,72 @@ const userRegister = async (req, res) => {
 
 }
 
+const loginUser = async (req, res) => {
+
+    const { email, password } = req.body
+
+    const existUser = await userModel.findOne({ email }).select("+password")
+
+    if (!existUser) {
+        return res.status(401).json({
+            message: "Invalid user"
+        })
+    }
+
+    const isPass = await bcrypt.compare(password, existUser.password)
+
+    if (!isPass) {
+        return res.status(401).json({
+            message: "Invalid password"
+        })
+    }
+
+    // create token
+    const token = await jwt.sign(
+        { userId: existUser._id },
+        process.env.JWT_SCRETE_KEY,
+        { expiresIn: "3d" }
+    )
+
+    // set to token into cookie
+    res.cookie("token", token)
+
+
+    res.status(202).json({
+        message: "user login successfulyy",
+        user: {
+            _id: existUser._id,
+            email: existUser.email,
+        },
+        token
+    })
+
+
+}
+
+const logoutUser = (req, res) => {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+    if (!token) {
+        return res.status(200).json({
+            message: "User logged out successfully"
+        })
+    }
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+
+
+}
+
+
+
 module.exports = {
-    userRegister
+    userRegister,
+    loginUser,
+    logoutUser
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const {userRegister} = require("../controllers/user.controller")
+const {userRegister, loginUser, logoutUser} = require("../controllers/user.controller")
 
 const authRouter = express.Router()
 
 authRouter.post("/register", userRegister)
+authRouter.post("/login", loginUser)
+authRouter.post("/logout", logoutUser)
 
 module.exports = authRouter
 
